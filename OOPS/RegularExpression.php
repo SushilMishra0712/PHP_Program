5a296a279bd4852d8c657699726e2b593fac56ee
Sushil Mishra <mishrasushil889@gmail.com>
2019-10-07 15:57:55 +0530

    regular expression

--- OOPS/RegularExpression.php
+++ OOPS/RegularExpression.php
@@ -6,7 +6,7 @@ class RegularExpression{
     function replaceData(){
         //message template
         $message="Hello <<name>>, We have your full name as <<full name>> 
-in our system. your contact number is 91-xxxxxxxxxx.
+in our system. Your contact number is 91-xxxxxxxxxx.
 Please,let us know in case of any clarification. 
 Thank you BridgeLabz 01-01-2016.";
         //user input details

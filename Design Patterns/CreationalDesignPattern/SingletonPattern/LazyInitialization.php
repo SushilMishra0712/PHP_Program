72a1e4c7bd7bb82a04f93427136ed5c487ac6b3c
Sushil Mishra <mishrasushil889@gmail.com>
2019-10-12 18:47:30 +0530

    lazy initialization concept

---
+++ Design Patterns/CreationalDesignPattern/SingletonPattern/LazyInitialization.php
@@ -0,0 +1,19 @@
+<?php
+class LazyInitializedSingleton{
+    private static $instance=null;
+    private function __construct(){
+
+    }
+    public static function getInstance(){
+        if(self::$instance==null){
+           self:: $instance=new LazyInitializedSingleton();
+        }
+        return self::$instance;
+    }
+}
+$object1=LazyInitializedSingleton::getInstance();
+var_dump($object1);
+$object2=LazyInitializedSingleton::getInstance();
+var_dump($object2);
+$object3=LazyInitializedSingleton::getInstance();
+var_dump($object3);

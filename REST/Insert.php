bcad91a50ac2448f1d5b3a53b9d10043873e9005
Sushil Mishra <mishrasushil889@gmail.com>
2019-10-21 18:18:45 +0530

    insert data

---
+++ REST/Insert.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>Insert data</title>
+</head>
+<body>
+<?php
+if(isset($_POST['submit'])){
+    $firstname=$_POST['firstname'];
+    $lastname=$_POST['lastname'];
+    $age=$_POST['age'];
+    $db=new PDO("mysql:host=localhost;dbname=test","root",null);
+    // echo "Connected with database test<br/>";
+    $query=$db->query("INSERT INTO `user`(`firstname`, `lastname`, `age`) VALUES('$firstname','$lastname','$age')");
+    echo "Data inserted successfully<br/>";
+}
+else{
+    echo "Form not submitted<br/>";
+}
+?>
+</body>
+</html>

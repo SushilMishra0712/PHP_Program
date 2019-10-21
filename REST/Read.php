6b657e9726c9b42be019bb7db62933b884f6d0eb
Sushil Mishra <mishrasushil889@gmail.com>
2019-10-21 18:19:24 +0530

    fetch data

---
+++ REST/Read.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+<style>
+td{
+    padding: 5px 20px;
+}
+</style>
+<title>Fetch data</title>
+</head>
+<body>
+<?php
+if(isset($_POST['fetch'])){
+    $table=$_POST['table'];
+    $db=new PDO("mysql:host=localhost;dbname=test","root",null);
+    $query=$db->query("SELECT * FROM $table");
+    if($query){
+        echo "<pre>";
+        echo "<table>";
+        echo "<tr>
+        <th>ID</th>
+        <th>FirstName</th>
+        <th>LastName</th>
+        <th>Age</th>
+        </tr>";
+        while($row=$query->fetch(PDO::FETCH_ASSOC)){
+            // print_r($row);
+            echo "<tr><td>".$row['Id']."</td><td>".$row['firstname']."</td><td>".$row['lastname']."</td><td>".$row['age']."</td></tr>";
+        }
+        echo "</table>";
+    }
+    else{
+        echo $table." table does not exits<br/>";
+    }
+}
+?>
+</body>
+</html>

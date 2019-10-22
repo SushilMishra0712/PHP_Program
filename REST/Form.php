b9bf043b48851200540ff440f8f3182f143ea82c
Sushil Mishra <mishrasushil889@gmail.com>
2019-10-22 17:28:50 +0530

    Form for user input

--- REST/Form.php
+++ REST/Form.php
@@ -5,7 +5,7 @@
 <body>
 
 <?php //form to insert data ?>
-<form action="insert.php" method="POST">
+<form action="RestApi.php" method="POST">
 <h3>Fill details to insert</h3>
 Firstname:&nbsp;<input type="text" name="firstname"><br/><br/>
 Lastname:&nbsp;<input type="text" name="lastname"><br/><br/>
@@ -14,21 +14,21 @@ Age&nbsp;:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="tex
 </form>
 
 <?php //form to delete data ?>
-<form action="delete.php" method="POST">
+<form action="RestApi.php" method="POST">
 <h3>Enter Id to delete user</h3> 
 <input type="text" name="id"><br/><br/>
 <input type="submit" name="delete" value="Delete"><br/><br/>
 </form>
 
 <?php //form to fetch data ?>
-<form action="fetch.php" method="POST">
+<form action="RestApi.php" method="POST">
 <h3>Enter table name to fetch data</h3>
 Tablename<input type="text" name="table"><br/><br/>
 <input type="submit" name="fetch" value="Fetch"><br/><br/>
 </form>
 
 <?php //form to update data ?>
-<form action="update.php" method="POST">
+<form action="RestApi.php" method="POST">
 <h3>Enter Id to update data</h3>
 Id:&nbsp;<input type="text" name="id"><br/><br/>
 <h4>Enter updated details</h4>

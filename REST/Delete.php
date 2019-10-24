8d2685689ba272954f2f2d61ae56328db0f42450
Sushil Mishra <mishrasushil889@gmail.com>
2019-10-24 18:31:55 +0530

    delete record

--- REST/Delete.php
+++ REST/Delete.php
@@ -1,35 +1,38 @@
 <html>
-<head>
-<title>Delete data</title>
-</head>
-<body>
-<?php
-if(isset($_POST['delete'])){
-    $id=$_POST['id'];
-    $servername = "localhost";
-    $username = "root";
-    $password = null;
-    $dbname = "test";
-    try {
-        $db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        // set the PDO error mode to exception
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-        // sql to delete a record
-        $sql = "DELETE FROM `user` WHERE id='$id'";
-    
-        // use exec() because no results are returned
-        $db->exec($sql);
-        echo "Record deleted successfully";
-        }
-    catch(PDOException $e)
+    <head>
+        <title>Delete data</title>
+    </head>
+    <body>
+        <?php
+        if(isset($_POST['delete']))
         {
-        echo $sql . "<br>" . $e->getMessage();
+            $id=$_POST['id'];
+            $servername = "localhost";
+            $username = "root";
+            $password = null;
+            $dbname = "test";
+            try 
+            {
+                $db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+                // set the PDO error mode to exception
+                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+                // sql to delete a record
+                $sql = "DELETE FROM `user` WHERE id='$id'";
+            
+                // use exec() because no results are returned
+                $db->exec($sql);
+                echo "Record deleted successfully";
+            }
+            catch(PDOException $e)
+            {
+                echo $sql . "<br>" . $e->getMessage();
+            }
         }
-}
-else{
-    echo "Form not submitted<br/>";
-}
-?>
-</body>
+        else
+        {
+            echo "Form not submitted<br/>";
+        }
+        ?>
+    </body>
 </html>

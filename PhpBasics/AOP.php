8cd14d3e893c63dbc42bce0bb8a490c5c9db9301
Sushil Mishra <mishrasushil889@gmail.com>
2019-10-24 10:12:18 +0530

    aspect oriented concept

--- PhpBasics/AOP.php
+++ PhpBasics/AOP.php
@@ -1,17 +1,18 @@
 <?php
 //Aspect oriented programming
-aop_add_before('MyServices->doAdmin*()','adviceForDoAdmin');
+// aop_add_before('MyServices->doAdmin*()','adviceForDoAdmin');
+session_start();
 class MyServices
 {
     public function doAdminStuff1()
     {
         //some stuff only admin should do
-        echo "Calling doAdminStuff1";
+        echo "Calling doAdminStuff1<br>";
     }
     public function doAdminStuff2()
     {
         //some stuff only admin should do
-        echo "Calling doAdminStuff2";
+        echo "Calling doAdminStuff2<br>";
     }
 }
 //this is called advice
@@ -22,23 +23,29 @@ function adviceForDoAdmin()
         throw new Exception('Sorry,you should be an admin to do this');
     }
 }
-
+$_SESSION['user-type']=null;
 //session is started & we added the above examples to configure MyServices & basicAdminChecker
 $services=new MyServices;
 try
-{
-    $services->doAdminStuff1();
+{   if($_SESSION['user-type']=='admin')
+    {
+       $services->doAdminStuff1();
+    }
+    else
+    {
+       echo "You Cannot access the service,you are not an Admin<br>";
+    }
 }
 catch(Exception $e)
 {
-    echo "You Cannot access the service,you are not an Admin";
+    echo "You Cannot access the service,you are not an Admin<br>";
 }
 //when user_type is admin
 $_SESSION['user_type']='admin';
 try
 {
-    $service->doAdminStuff1();
-    $service->doAdminStuff2();
+    $services->doAdminStuff1();
+    $services->doAdminStuff2();
 }
 catch(Exception $e)
 {

633619e01296bb2c5a162a0e367f371fec4de22b
Sushil Mishra <mishrasushil889@gmail.com>
2019-10-31 14:21:29 +0530

    insert new record

--- REST/Insert.php
+++ REST/Insert.php
@@ -1,23 +1,16 @@
-<html>
-    <head>
-        <title>Insert data</title>
-    </head>
-    <body>
-        <?php
-        if(isset($_POST['submit']))
-        {
-            $firstname=$_POST['firstname'];
-            $lastname=$_POST['lastname'];
-            $age=$_POST['age'];
-            $db=new PDO("mysql:host=localhost;dbname=test","root",null);
-            // echo "Connected with database test<br/>";
-            $query=$db->query("INSERT INTO `user`(`firstname`, `lastname`, `age`) VALUES('$firstname','$lastname','$age')");
-            echo "Data inserted successfully<br/>";
-        }
-        else
-        {
-            echo "Form not submitted<br/>";
-        }
-        ?>
-    </body>
-</html>
+<?php
+if(isset($_POST['submit']))
+{
+    $firstname=$_POST['firstname'];
+    $lastname=$_POST['lastname'];
+    $age=$_POST['age'];
+    $db=new PDO("mysql:host=localhost;dbname=test","root",null);
+    // echo "Connected with database test<br/>";
+    $query=$db->query("INSERT INTO `user`(`firstname`, `lastname`, `age`) VALUES('$firstname','$lastname','$age')");
+    echo "Data inserted successfully<br/>";
+}
+else
+{
+    echo "Form not submitted<br/>";
+}
+?>

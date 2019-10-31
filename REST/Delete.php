41fc6e5ae520a22c05fe62dd3fb985ad940f3075
Sushil Mishra <mishrasushil889@gmail.com>
2019-10-31 14:20:03 +0530

    delete record

--- REST/Delete.php
+++ REST/Delete.php
@@ -1,38 +1,31 @@
-<html>
-    <head>
-        <title>Delete data</title>
-    </head>
-    <body>
-        <?php
-        if(isset($_POST['delete']))
-        {
-            $id=$_POST['id'];
-            $servername = "localhost";
-            $username = "root";
-            $password = null;
-            $dbname = "test";
-            try 
-            {
-                $db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-                // set the PDO error mode to exception
-                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+<?php
+if(isset($_POST['delete']))
+{
+    $id=$_POST['id'];
+    $servername = "localhost";
+    $username = "root";
+    $password = null;
+    $dbname = "test";
+    try 
+    {
+        $db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             
-                // sql to delete a record
-                $sql = "DELETE FROM `user` WHERE id='$id'";
+        // sql to delete a record
+        $sql = "DELETE FROM `user` WHERE id='$id'";
             
-                // use exec() because no results are returned
-                $db->exec($sql);
-                echo "Record deleted successfully";
-            }
-            catch(PDOException $e)
-            {
-                echo $sql . "<br>" . $e->getMessage();
-            }
-        }
-        else
-        {
-            echo "Form not submitted<br/>";
-        }
-        ?>
-    </body>
-</html>
+        // use exec() because no results are returned
+        $db->exec($sql);
+        echo "Record deleted successfully";
+    }
+    catch(PDOException $e)
+    {
+        echo $sql . "<br>" . $e->getMessage();
+    }
+}
+else
+{
+    echo "Form not submitted<br/>";
+}
+?>

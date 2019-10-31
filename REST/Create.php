24b9979dc4d0121e7f1e87f5069a36e8877ece0f
Sushil Mishra <mishrasushil889@gmail.com>
2019-10-31 14:19:00 +0530

    create new table

--- REST/Create.php
+++ REST/Create.php
@@ -1,22 +1,15 @@
-<html>
-    <head>
-        <title>Create table</title>
-    </head>
-    <body>
-        <?php
-            $db=new PDO("mysql:host=localhost;dbname=myDBPDO","root",null);
-            echo "Connected with database\n";
-            //create database myDBPDO
-            // $sql="CREATE DATABASE myDBPDO";
-            // $db->exec($sql);
-            //create table MyGuests
-            $sql="CREATE TABLE MyGuests(id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-                                        firstname VARCHAR(30) NOT NULL,
-                                        lastname VARCHAR(30) NOT NULL,
-                                        email VARCHAR(50),
-                                        reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)";
-            $db->exec($sql);
-            echo "Table Created Succesfully";
-        ?>
-    </body>
-</html>
+<?php
+    $db=new PDO("mysql:host=localhost;dbname=myDBPDO","root",null);
+    echo "Connected with database\n";
+    //create database myDBPDO
+    // $sql="CREATE DATABASE myDBPDO";
+    // $db->exec($sql);
+    //create table MyGuests
+    $sql="CREATE TABLE MyGuests(id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                                firstname VARCHAR(30) NOT NULL,
+                                lastname VARCHAR(30) NOT NULL,
+                                email VARCHAR(50),
+                                reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)";
+    $db->exec($sql);
+    echo "Table Created Succesfully";
+?>

9da33780761a06da70e92c8fbfa2193d34badc43
Sushil Mishra <mishrasushil889@gmail.com>
2019-10-31 14:20:52 +0530

    form for user action

--- REST/Form.php
+++ REST/Form.php
@@ -3,8 +3,7 @@
         <title>User Details</title>
     </head>
     <body>
-
-    <?php //form to insert data ?>
+        
         <form action="RestApi.php" method="POST">
             <h3>Fill details to insert</h3>
             Firstname:&nbsp;<input type="text" name="firstname"><br/><br/>
@@ -13,22 +12,19 @@
             <input type="submit" name="submit"><br/><br/><br/>
         </form>
 
-    <?php //form to delete data ?>
         <form action="RestApi.php" method="POST">
             <h3>Enter Id to delete user</h3> 
             <input type="text" name="id"><br/><br/>
             <input type="submit" name="delete" value="Delete"><br/><br/>
         </form>
 
-    <?php //form to fetch data ?>
         <form action="RestApi.php" method="POST">
             <h3>Enter table name to fetch data</h3>
             Tablename<input type="text" name="table"><br/><br/>
             <input type="submit" name="fetch" value="Fetch"><br/><br/>
         </form>
 
-    <?php //form to update data ?>
-            <form action="RestApi.php" method="POST">
+        <form action="RestApi.php" method="POST">
             <h3>Enter Id to update data</h3>
             Id:&nbsp;<input type="text" name="id"><br/><br/>
             <h4>Enter updated details</h4>

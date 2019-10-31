3886e48ebaf34fc2f25831aa34512df1da3aad2d
Sushil Mishra <mishrasushil889@gmail.com>
2019-10-31 14:38:43 +0530

    backend files

--- Week6/Backend/login_validate.php
+++ Week6/Backend/login_validate.php
@@ -8,41 +8,44 @@ class login_validate
         {
             $email=$_POST['email'];
             $password=$_POST['password'];
-            //sanitize and validate an email address
-            // $email=filter_var($email,FILTER_SANITISE_EMAIL);
-            // if(!filter_var($email,FILTER_VALIDATE_EMAIL)==true)
-            // {
-                $db=new PDO("mysql:host=localhost;dbname=test","root",null);
-                
-                $query=$db->query('SELECT * FROM Userlogin');
-                echo "<pre>";
-                $flag="false";
-                while($row=$query->fetch(PDO::FETCH_ASSOC))
-                {
-                    if($row['email']== $email && $row['password']==$password)
+            if (isset($_POST['email'])) 
+            { 
+                if (!filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL) === false) 
+                { 
+                    $db=new PDO("mysql:host=localhost;dbname=test","root",null);
+                    
+                    $query=$db->query('SELECT * FROM Userlogin');
+                    echo "<pre>";
+                    $flag="false";
+                    while($row=$query->fetch(PDO::FETCH_ASSOC))
                     {
-                        $flag="true";
-                        $name=$row['firstname'];
-                        $_SESSION['email']=$email;
-                        $_SESSION['user']=$row['firstname']." ".$row['lastname'];
-                        $_SESSION['log']='1';
+                        if($row['email']== $email && $row['password']==$password)
+                        {
+                            $flag="true";
+                            $name=$row['firstname'];
+                            $_SESSION['email']=$email;
+                            $_SESSION['user']=$row['firstname']." ".$row['lastname'];
+                            $_SESSION['log']='1';
+                        }
+                    }
+                    if($flag=="false")
+                    {
+                        echo "<script>window.alert('Email and password does not match');window.location.href='../Frontend/login.php';</script><br>";
+                    }
+                    else if($flag=="true")
+                    {
+                        header("Location:../Frontend/home.php");
                     }
                 }
-                if($flag=="false")
+                else
                 {
-                    echo "<script>window.alert('Email and password does not match');window.location.href='../Frontend/login.php';</script><br>";
+                    echo "<script>window.alert('Email is not a valid email address');window.location.href='../Frontend/login.php';</script><br>";
                 }
-                else if($flag=="true")
-                {
-                    header("Location:../Frontend/home.php");
-                }
-            // }
-            // else
-            // {
-            //     echo "<script>window.alert('Email is not a valid email address');window.location.href='../Frontend/login.php';</script><br>";
-            // }
+            }
+           
         }
-        else{
+        else
+        {
             echo "Form does not submitted<br>";
         }
     }

e536a08d3bbaee27fd843e35b5fbaa98d3aa9c15
Sushil Mishra <mishrasushil889@gmail.com>
2019-11-08 19:23:00 +0530

    remove yii folder

--- Login/Backend/loginUser.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'User.php';
-$object_login_validate=new User;
-$object_login_validate->login();
-?>
-
-
-
-
--- Login/Backend/registrationUser.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'User.php';
-$object_registration_validate=new User;
-$object_registration_validate->create_table_if_notexist();
-$object_registration_validate->registration();
-?>

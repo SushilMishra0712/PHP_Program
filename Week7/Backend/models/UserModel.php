3b1f6a07b18b16f01f58f1aa17a3a91fbd0f27f7
Sushil Mishra <mishrasushil889@gmail.com>
2019-11-08 19:23:51 +0530

    week7 code

---
+++ Week7/Backend/models/UserModel.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace app\models;
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\Login;
+
+/**
+ * LoginUserSearch represents the model behind the search form of `app\models\Login`.
+ */
+class UserModel extends Login
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'age', 'phone_number'], 'integer'],
+            [['firstname', 'lastname', 'email', 'password', 'address'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Login User
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function checkUser($email,$pass)
+    {
+        $user = (new \yii\db\Query())
+                ->select('*')
+                ->from('login')
+                ->where(['email'=>$email,'password'=>$pass])
+                ->one();
+        return $user;
+    }
+
+    /**
+     * Register user
+     */
+    public function registerUser($firstname,$lastname,$email,$pass,$age,$address,$phonenumber)
+    {
+        $user=Yii::$app->db->createCommand()
+              ->insert('login', [
+            'firstname' => $firstname,
+            'lastname' => $lastname,
+            'email' => $email,
+            'password' => $pass,
+            'age' => $age,
+            'address' => $address,
+            'phone_number' => $phonenumber,
+            ])->execute();
+       return $user;
+    }
+
+    public function updateUser()
+    {
+        
+    }
+
+}
